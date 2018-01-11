69498a04bba41d197f47a381b06615a21a19b5de
Mani Kumar <mk0872191152@gmail.com>
2018-01-12 00:01:05 +0530

    Add padding and match all activities

--- app/src/main/java/com/example/mani/scanner/EntryExitActivity.java
+++ app/src/main/java/com/example/mani/scanner/EntryExitActivity.java
@@ -7,18 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class EntryExitActivity extends AppCompatActivity implements View.OnClickListener{
+public class EntryExitActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String Data;
     private Button mEnter, mExit, mBulkExit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_exit);
 
-        mEnter = (Button)findViewById(R.id.btn_enter);
-        mExit = (Button)findViewById(R.id.btn_exit);
-        mBulkExit = (Button)findViewById(R.id.btn_bulkExit);
+        mEnter = (Button) findViewById(R.id.btn_enter);
+        mExit = (Button) findViewById(R.id.btn_exit);
+        mBulkExit = (Button) findViewById(R.id.btn_bulkExit);
 
         mEnter.setOnClickListener(this);
         mExit.setOnClickListener(this);
@@ -30,20 +31,20 @@ public class EntryExitActivity extends AppCompatActivity implements View.OnClick
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_enter:
                 Intent entryIntent = new Intent(this, VolleyRequestActivity.class);
-                entryIntent.putExtra("scanType",Data+"Entry");
+                entryIntent.putExtra("scanType", Data + "Entry");
                 startActivity(entryIntent);
                 break;
             case R.id.btn_exit:
                 Intent exitIntent = new Intent(this, VolleyRequestActivity.class);
-                exitIntent.putExtra("scanType", Data+"exit");
+                exitIntent.putExtra("scanType", Data + "exit");
                 startActivity(exitIntent);
                 break;
             case R.id.btn_bulkExit:
                 Intent bulkExitIntent = new Intent(this, VolleyRequestActivity.class);
-                bulkExitIntent.putExtra("scanType",Data);
+                bulkExitIntent.putExtra("scanType", Data);
                 startActivity(bulkExitIntent);
                 break;
         }
