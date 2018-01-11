b0df00f69b8a36a014dfe5e3f2aceb3cff5e948f
Mani Kumar <mk0872191152@gmail.com>
2018-01-12 00:59:48 +0530

    Reformat code and update string.xml file

--- app/src/main/java/com/example/mani/scanner/HallNumberActivity.java
+++ app/src/main/java/com/example/mani/scanner/HallNumberActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class HallNumberActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String HALL1 = "hall1";
+    private static final String HALL2 = "hall2";
+    private static final String HALL3 = "hall3";
+    private static final String HALL4 = "hall4";
+    private static final String HALL5 = "hall5";
 
     private Button mHall1, mHall2, mHall3, mHall4, mHall5;
 
@@ -34,27 +39,27 @@ public class HallNumberActivity extends AppCompatActivity implements View.OnClic
         switch (view.getId()) {
             case R.id.btn_hall1:
                 Intent hall1Intent = new Intent(this, EntryExitActivity.class);
-                hall1Intent.putExtra("hallNumber", "hall1");
+                hall1Intent.putExtra("hallNumber", HALL1);
                 startActivity(hall1Intent);
                 break;
             case R.id.btn_hall2:
                 Intent hall2Intent = new Intent(this, EntryExitActivity.class);
-                hall2Intent.putExtra("hallNumber", "hall2");
+                hall2Intent.putExtra("hallNumber", HALL2);
                 startActivity(hall2Intent);
                 break;
             case R.id.btn_hall3:
                 Intent hall3Intent = new Intent(this, EntryExitActivity.class);
-                hall3Intent.putExtra("hallNumber", "hall3");
+                hall3Intent.putExtra("hallNumber", HALL3);
                 startActivity(hall3Intent);
                 break;
             case R.id.btn_hall4:
                 Intent hall4Intent = new Intent(this, EntryExitActivity.class);
-                hall4Intent.putExtra("hallNumber", "hall4");
+                hall4Intent.putExtra("hallNumber", HALL4);
                 startActivity(hall4Intent);
                 break;
             case R.id.btn_hall5:
                 Intent hall5Intent = new Intent(this, EntryExitActivity.class);
-                hall5Intent.putExtra("hallNumber", "hall5");
+                hall5Intent.putExtra("hallNumber", HALL5);
                 startActivity(hall5Intent);
                 break;
         }
